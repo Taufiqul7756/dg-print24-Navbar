c767bdf6eefbc79ba60c5e153bcd7725895644cf
Taufiqul7756 <taufiqulislam.official@.com>
2024-08-03 13:25:38 +0600

    1.0.2

--- README.md
+++ README.md
@@ -6,6 +6,13 @@
   - Navbar - Running
 
 - version 1.0.1
+
   - Day - 03-08-2024
   - Navbar - 1st section (LogoBar) Done
   - Navbar - 2nd section (All Products navitems) Done
+
+- version 1.0.2
+  - Navbar - 1st section (LogoBar) Done
+  - Navbar - 2nd section (All Products navitems) Done
+  - Navbar - 3nd section (Ticket navitems) Done
+  - responsive for md and sm ( Fake data not loaded yet)
--- src/app/Components/Navbar/Navbar.jsx
+++ src/app/Components/Navbar/Navbar.jsx
@@ -20,6 +20,10 @@ import {
   Modal,
   ModalContent,
   ModalBody,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  useDisclosure,
 } from "@nextui-org/react";
 
 import Image from "next/image.js";
@@ -38,6 +42,12 @@ import logo from "../../../../public/logo.png";
 export default function NavbarComponent() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
   const [userData, setUserData] = React.useState(false);
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const {
+    isOpen: isOpenModal2,
+    onOpen: onOpenModal2,
+    onOpenChange: onOpenChangeModal2,
+  } = useDisclosure();
 
   return (
     <Navbar maxWidth="2xl" height={"11rem"} onMenuOpenChange={setIsMenuOpen}>
@@ -65,9 +75,9 @@ export default function NavbarComponent() {
             size="lg"
             startContent={<CiSearch size={24} />}
             type="search"
-            // classNames={{
-            //   base: "col-span-1",
-            // }}
+            classNames={{
+              base: "col-span-1",
+            }}
           />
 
           {/* -------------- Login or logout -------------- */}
@@ -113,7 +123,7 @@ export default function NavbarComponent() {
                     radius="sm"
                     disableRipple
                     className="hidden md:flex md:justify-center border-[#ED1F24] hover:bg-[#ED1F24] hover:text-white"
-                    // onClick={onOpenModal2}
+                    onClick={onOpenModal2}
                   >
                     LOGIN
                   </Button>
@@ -123,7 +133,7 @@ export default function NavbarComponent() {
                     radius="sm"
                     disableRipple
                     className="bg-black text-white border-none hover:bg-[#ED1F24] hover:text-white"
-                    // onClick={onOpenModal2}
+                    onClick={onOpenModal2}
                   >
                     REGISTER FOR FREE
                   </Button>
@@ -188,14 +198,14 @@ export default function NavbarComponent() {
                 radius="none"
                 variant="light"
                 endContent={<FaChevronDown className="animate-pulse" />}
-                // onClick={onOpen}
+                onClick={onOpen}
                 className="text-white"
               >
                 All Products
               </Button>
               <Modal
-                // isOpen={isOpen}
-                // onOpenChange={onOpenChange}
+                isOpen={isOpen}
+                onOpenChange={onOpenChange}
                 size="5xl"
                 placement="top"
                 backdrop="opaque"
@@ -267,8 +277,97 @@ export default function NavbarComponent() {
               ))}
             </div> */}
           </div>
+
+          {/* navbar -- create support ticket */}
+          <div className="flex flex-wrap items-center justify-center gap-10 py-4 ">
+            <Link
+              href="#"
+              className="flex items-center space-x-2 text-sm w-full sm:w-1/2 md:w-auto text-black"
+            >
+              <span className="">
+                <MdOutlineEmail size={20} />
+              </span>
+              <span>Create a support ticket</span>
+            </Link>
+            <div className="border-r h-6 hidden md:block border-black"></div>
+            <Link
+              href="#"
+              className="flex items-center space-x-2 text-sm w-full sm:w-1/2 md:w-auto text-black"
+            >
+              <span className="">
+                <FaBookOpenReader className="" size={20} />
+              </span>
+              <span>Dgprint24 University</span>
+            </Link>
+            <div className="border-r h-6 hidden md:block border-black"></div>
+            <Link
+              href="#"
+              className="flex items-center space-x-2 text-sm w-full sm:w-1/2 md:w-auto text-black"
+            >
+              <span className="">
+                <PiChatsThin size={20} />
+              </span>
+              <span>Chat with an agent</span>
+            </Link>
+            <div className="border-r h-6 hidden md:block border-black"></div>
+            <Link
+              href="#"
+              className="flex items-center space-x-2 text-sm w-full sm:w-1/2 md:w-auto text-black"
+            >
+              <span className="">
+                <TfiHeadphone size={20} />
+              </span>
+              <span>Go to help center</span>
+            </Link>
+          </div>
         </div>
       </NavbarContent>
+
+      {/* //!!hamburger icon open for medium screen ------------------- */}
+
+      <div className="  lg:hidden w-full">
+        {/* icon , logo and cart icon */}
+        <NavbarContent className="py-4 gap-0 justify-evenly">
+          <NavbarMenuToggle
+            aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+            icon={<RiMenu4Fill size={30} />}
+            className="m-3"
+          />
+          <NavbarBrand className="justify-center">
+            <Link href="/" color="foreground">
+              <Image
+                as={logo}
+                width={100}
+                height={50}
+                src={logo}
+                alt="Tech analytica's logo"
+                radius="none"
+                isBlurred={false}
+              />
+            </Link>
+          </NavbarBrand>
+          <Button
+            href="/cart"
+            as={Link}
+            isIconOnly
+            variant="light"
+            disableRipple
+            size="lg"
+          >
+            <IoCartOutline size={28} />
+          </Button>
+        </NavbarContent>
+        <NavbarContent>
+          <Input
+            radius="sm"
+            variant="bordered"
+            placeholder="Search in DG Print..."
+            size="lg"
+            startContent={<CiSearch size={24} />}
+            type="search"
+          />
+        </NavbarContent>
+      </div>
 
       {/* <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem>
@@ -300,6 +399,157 @@ export default function NavbarComponent() {
       {/* <NavbarMenu>
         
       </NavbarMenu> */}
+
+      {/* //! MOBILE MENU */}
+
+      <NavbarMenu>
+        <NavbarContent
+          justify="center"
+          scrollShadowProps={{
+            hideScrollBar: false,
+          }}
+          className="overflow-x-auto scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-gray-400"
+        >
+          <NavbarContent className="flex flex-col md:flex-row md:justify-center space-x-4 ">
+            <div id="allProducts">
+              <Popover placement="bottom">
+                <PopoverTrigger>
+                  <Button
+                    disableRipple
+                    disableAnimation
+                    radius="none"
+                    variant="light"
+                    endContent={<FaChevronDown className="animate-pulse" />}
+                  >
+                    All Products
+                  </Button>
+                </PopoverTrigger>
+
+                {/* <PopoverContent className="grid grid-cols-1 gap-4 max-h-60 overflow-scroll">
+                  {allProducts.map((category, index) => (
+                    <div key={index}>
+                      <p className=" text-lg font-extrabold">
+                        {category.categoryname}
+                      </p>
+                      {category.products.map((product, i) => (
+                        <Link
+                          href={`/category/${category.categoryId}/${product.productId}`}
+                          key={i}
+                          className="flex "
+                        >
+                          <p className="text-black">{product.label}</p>
+                        </Link>
+                      ))}
+                    </div>
+                  ))}
+                </PopoverContent> */}
+              </Popover>
+            </div>
+            {/* <div className="grid">
+              {allProducts.map((category, index) => (
+                <Dropdown radius="none" key={index}>
+                  <DropdownTrigger>
+                    <Button
+                      disableRipple
+                      disableAnimation
+                      radius="none"
+                      variant="light"
+                      endContent={<FaChevronDown />}
+                    >
+                      {category.categoryname}
+                    </Button>
+                  </DropdownTrigger>
+                  <DropdownMenu
+                    aria-label="Static Actions"
+                    className="max-h-60 overflow-scroll"
+                  >
+                    {category.products.map((item, i) => (
+                      <DropdownItem
+                        key={i}
+                        href={`/category/${category.categoryId}/${item.productId}`}
+                      >
+                        {item.label}
+                      </DropdownItem>
+                    ))}
+                  </DropdownMenu>
+                </Dropdown>
+              ))}
+            </div> */}
+            <NavbarItem className="flex-shrink-0">
+              {!userData && (
+                <div className="flex flex-col gap-4">
+                  <Button
+                    variant="bordered"
+                    size="md"
+                    radius="sm"
+                    disableRipple
+                    className=""
+                    onClick={onOpenModal2}
+                  >
+                    LOGIN
+                  </Button>
+                  <Button
+                    variant="bordered"
+                    size="md"
+                    radius="sm"
+                    disableRipple
+                    className=""
+                    onClick={onOpenModal2}
+                  >
+                    REGISTER FOR FREE
+                  </Button>
+                  <Modal
+                    isOpen={isOpenModal2}
+                    onOpenChange={onOpenChangeModal2}
+                    backdrop="opaque"
+                    radius="none"
+                    disableAnimation
+                  >
+                    <ModalContent className="bg-transparent">
+                      {/* <FormsTab /> */}
+                    </ModalContent>
+                  </Modal>
+                </div>
+              )}
+              {/* {userData.accessToken && (
+                <Dropdown placement="bottom-end">
+                  <DropdownTrigger>
+                    <User
+                      name={userData.name}
+                      description={userData.email}
+                      avatarProps={{
+                        src: userData.picture,
+                        showFallback: true,
+                        name: userData.name,
+                      }}
+                      className="hover:drop-shadow-lg hover:cursor-pointer"
+                    />
+                  </DropdownTrigger>
+                  <DropdownMenu aria-label="Profile Actions" variant="flat">
+                    <DropdownItem key="myaccount" href="/profile/manage">
+                      My Account
+                    </DropdownItem>
+                    <DropdownItem key="myorder" href="/profile/orders">
+                      My Order
+                    </DropdownItem>
+                    <DropdownItem key="logout">
+                      <Button
+                        fullWidth
+                        color="danger"
+                        type="submit"
+                        isLoading={loading}
+                        onClick={handleLogout}
+                      >
+                        Log Out
+                      </Button>
+                    </DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
+              )} */}
+            </NavbarItem>
+          </NavbarContent>
+        </NavbarContent>
+      </NavbarMenu>
     </Navbar>
   );
 }
